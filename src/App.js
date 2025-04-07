1798997b83c1a8b7ba8c5cea7a44f7eedcee3739
MohamedAshraf366 <40272921+MohamedAshraf366@users.noreply.github.com>
2025-04-07 09:37:10 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import Home from './component/home';
-
-
 
 function App() {
   return (
-    <BrowserRouter basename='/Project10-React-Roberto'>
-
-    <Routes>
-      <Route path='/' element = {<Home />} />
-    </Routes>
-    </BrowserRouter>
-    
-    
-    
+    <HashRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </HashRouter>
   );
 }
 
 export default App;
-
-
-
